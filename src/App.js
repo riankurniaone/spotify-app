2e49e3a7073302d5137c87ee51af6f15e997ebd5
Rian Kurniawan <riankurcyberdeepweb@gmail.com>
2022-03-27 15:05:59 +0700

    feature card playlist use data.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import data from "./data.js";
 
 function App() {
   const API_KEY = process.env.REACT_APP_API_KEY;
@@ -7,9 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello World!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +18,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="container">
+      <h1>My Playlist</h1>
+      <div class="cardPlaylist">
+        <img src={data.album.images[0].url} alt="ImagePlaylist" />
+        <h3>{data.album.name}</h3>
+        <p>{data.album.artists[0].name}</p>
+        <button class="btnSelect">Select</button>
+      </div>
+    </div>
     </div>
   );
 }
