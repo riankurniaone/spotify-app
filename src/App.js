daf641e1762c014dc71b3f0ef82c26c1c638d822
Rian Kurniawan <riankurcyberdeepweb@gmail.com>
2022-03-27 19:52:34 +0700

    looping track playlist

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import Track from "./components/Track";
 
 function App() {
   // const API_KEY = process.env.REACT_APP_API_KEY;
+
+  const trackList = data.map((track) => (
+    <Track
+      key={track.id}
+      url={track.album.images[0].url}
+      title={track.name}
+      artist={track.artists[0].name}
+    />
+  ));
+
   return (
       <div className="container">
-      <h1>My Playlist</h1>
-      <div class="cardPlaylist">
-        <img src={data.album.images[0].url} alt="ImagePlaylist" />
-        <h3>{data.album.name}</h3>
-        <p>{data.album.artists[0].name}</p>
-        <button class="btnSelect">Select</button>
-      </div>
-      <Track
-        url={data.album.images[0].url}
-        title={data.album.name}
-        artist={data.album.artists[0].name}
-      />
+      <h1>Playlist Spotify</h1>
+      <div className="trackList">{trackList}</div>
     </div>
   );
 }
