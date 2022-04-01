7a2729ad5e3ac4d5abe7bfb1433cfbf44c2cba60
Rian Kurniawan <riankurcyberdeepweb@gmail.com>
2022-04-01 14:01:34 +0700

    homework : select and deselect songs spotify

--- src/components/Track/index.js
+++ src/components/Track/index.js
@@ -1,12 +1,26 @@
-const Track = ({ url, title, artist }) => {
+import React, { useState } from "react";
+
+export default function Track({ url, title, artist, toggleSelect }) {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleSelect = () => {
+    setIsSelected(!isSelected);
+    toggleSelect();
+  };
+
     return (
       <div className="card-playlist">
         <img src={url} alt="Track Playlist" />
         <h3>{title}</h3>
         <p>{artist}</p>
-        <button className="btn-primary btn-select">Select</button>
+        <button
+        className={`btn btn-select ${
+          isSelected ? "btn-primary" : "btn-secondary"
+        }`}
+        onClick={handleSelect}
+      >
+        {isSelected ? "Deselect" : "Select"}
+      </button>
       </div>
     );
-  };
-  
-  export default Track;
+      }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,68 +1,112 @@
-import React, { Component } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
 import Track from "../../components/Track";
-import Searchbar from "../../components/Searchbar";
+import SearchBar from "../../components/Searchbar";
 import config from "../../utils/config";
 
-export default class Home extends Component {
-  state = {
-    accessToken: "",
-    isAuthorize: false,
-    tracks: [],
-  };
+export default function Home() {
+  const [tracks, setTracks] = useState([]);
+  const [accessToken, setAccessToken] = useState("");
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [selectedTrackURI, setSelectedTrackURI] = useState([]);
+  const [isSearch, setIsSearch] = useState(false);
 
-  componentDidMount() {
-    const accessToken = new URLSearchParams(window.location.hash).get(
-      "#access_token"
-    );
+// useEffect for getting access token from hash url with URLSearchParams
+useEffect(() => {
+  const params = new URLSearchParams(window.location.hash);
+  const accessToken = params.get("#access_token");
+  setAccessToken(accessToken);
+  setIsAuthorized(accessToken !== null);
+}, []);
 
-    this.setState({ accessToken, isAuthorize: accessToken !== null });
-  }
+// kalau lagi ga search (kondisi false), selectedTracknya dimasukin ke state Tracks
+useEffect(() => {
+  if (!isSearch) {
+    const selectedTracks = filterSelectedTracks();
 
-  getSpotifyLinkAuthorize() {
+    setTracks(selectedTracks);
+    }
+  }, [selectedTrackURI]);
+  // trigger - jalan kalau selectedTrackURI berubah tapi di kondisi search is false
+
+  const getSpotifyLinkAuthorize = () => {
     const state = Date.now().toString();
     const clientId = process.env.REACT_APP_SPOTIFY_ID;
 
-    return `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=http://localhost:3000&state=${state}&scope=${config.SPOTIFY_SCOPE}`;
-  }
+    return `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=${config.RESPONSE_TYPE}&redirect_uri=${config.REDIRECT_URI}&state=${state}&scope=${config.SPOTIFY_SCOPE}`;
+  };
 
-  handleSuccessSearch(tracks) {
-    this.setState({ tracks });
-  }
+  // untuk memfilter data track berdasarkan URI yang ada
+  const filterSelectedTracks = () => {
+    return tracks.filter((track) => selectedTrackURI.includes(track.uri));
+  };
 
-  render() {
-    return (
-      <div className="container">
-        {!this.state.isAuthorize && (
-          <div className="login-app">
-            <p>Sebelum menggunakan aplikasi, silakan login ke Spotify disini.</p>
-            <a href={this.getSpotifyLinkAuthorize()} className="btn-primary">
-              Login
-            </a>
-          </div>
-        )}
+  // result tracks from search
+  const handleSuccessSearch = (searchTracks) => {
+    setIsSearch(true);
+    const selectedTracks = filterSelectedTracks();
 
-        {this.state.isAuthorize && (
-          <>
-            <h1>Spotify Playlist</h1>
-            <Searchbar
-              accessToken={this.state.accessToken}
-              onSuccess={(tracks) => this.handleSuccessSearch(tracks)}
-            />
-            {this.state.tracks.length === 0 && <p>No tracks</p>}
-
-            <div className="track-list">
-              {this.state.tracks.map((track) => (
-                <Track
-                  key={track.id}
-                  url={track.album.images[0].url}
-                  title={track.name}
-                  artist={track.artists[0].name}
-                />
-              ))}
-            </div>
-          </>
-        )}
-      </div>
+    // distinct agar data ga double antara track yang udah deselected dengan tracks hasil search
+    const searchDistinctTracks = searchTracks.filter(
+      (track) => !selectedTrackURI.includes(track.uri)
     );
-  }
+  
+    // ... spread operator untuk memecah array
+    setTracks([...selectedTracks, ...searchDistinctTracks]);
+  };
+
+  const clearSearch = () => {
+    const selectedTracks = filterSelectedTracks();
+
+    setTracks(selectedTracks);
+    setIsSearch(false);
+  };
+
+  const toggleSelect = (track) => {
+    const uri = track.uri;
+
+    if (selectedTrackURI.includes(uri)) {
+      setSelectedTrackURI(selectedTrackURI.filter((item) => item !== uri));
+    } else {
+      setSelectedTrackURI([...selectedTrackURI, uri]);
+    }
+  };
+
+  return (
+    <div className="container">
+      {!isAuthorized && (
+        <div className="login-app">
+          <p>Before using the app, please login to Spotify here.</p>
+          <a href={getSpotifyLinkAuthorize()} className="btn btn-primary">
+            Login
+          </a>
+        </div>
+      )}
+
+      {isAuthorized && (
+        <>
+          <h1>Spotify Playlist</h1>
+          <SearchBar
+            accessToken={accessToken}
+            onSuccess={(tracks) => handleSuccessSearch(tracks)}
+            onClearSearch={clearSearch}
+          />
+
+          {tracks.length === 0 && <p>No tracks</p>}
+
+          <div className="track-list">
+            {tracks.map((track) => (
+              <Track
+                key={track.id}
+                url={track.album.images[0].url}
+                title={track.name}
+                artist={track.artists[0].name}
+                toggleSelect={() => toggleSelect(track)}
+              />
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
