b31dba96956e19650ebfa6bd2188a0d0143eb150
Rian Kurniawan <riankurcyberdeepweb@gmail.com>
2022-04-18 12:32:09 +0700

    repair and use typescript

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -8,7 +8,7 @@ interface IConfig {
 }
 
 const config: IConfig = {
-  API_URL: process.env.REACT_APP_API_KEY,
+  API_URL: process.env.REACT_APP_SPOTIFY_ID,
   SPOTIFY_AUTH_URL: "https://accounts.spotify.com/authorize",
   SPOTIFY_BASE_URL: "https://api.spotify.com/v1",
   SPOTIFY_SCOPE: "playlist-modify-private",
