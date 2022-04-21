c90927509b77b469c6b06c5521d8a0f2b04b0adb
Rian Kurniawan <riankurcyberdeepweb@gmail.com>
2022-04-21 17:31:43 +0700

    update config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,10 @@
+const getHost: () => string = () => {
+  const protocol: string = window.location.protocol;
+  const host: string = window.location.host;
+
+  return `${protocol}//${host}`;
+};
+
 interface IConfig {
   API_URL: string | undefined;
   SPOTIFY_AUTH_URL: string;
@@ -13,7 +20,7 @@ const config: IConfig = {
   SPOTIFY_BASE_URL: "https://api.spotify.com/v1",
   SPOTIFY_SCOPE: "playlist-modify-private",
   RESPONSE_TYPE: "token",
-  REDIRECT_URI: "http://localhost:3000",
+  REDIRECT_URI: getHost(),
 };
 
 export default config;
